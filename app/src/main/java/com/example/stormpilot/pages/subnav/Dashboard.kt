4817c09be5b1e5137fe0a211148ee74bbea972a7
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2026-03-18 11:36:12 -0400

    Start

--- app/src/main/java/com/example/stormpilot/pages/subnav/Dashboard.kt
+++ app/src/main/java/com/example/stormpilot/pages/subnav/Dashboard.kt
@@ -1,16 +1,120 @@
 package com.example.stormpilot.pages.subnav
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.material.icons.rounded.Terminal
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose.StormPilotTheme
 
 @Composable fun RadarPage() {
-    var isDarkMode by remember { mutableStateOf(false) }
+    var isDarkMode by remember { mutableStateOf(true) }
 
     StormPilotTheme(darkTheme = isDarkMode) {
+        Column{
+            LocationWidget("Greensburg, Kansas")
+            Spacer(modifier = Modifier.height(15.dp))
+            AlertsWidget(flood = true, thunder = true, tornado = false)
+        }
+    }
+}
+
+@Composable fun LocationWidget(location: String) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp, top = 20.dp),
+        shape = RoundedCornerShape(14.dp),
+        color = MaterialTheme.colorScheme.surfaceContainer,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(start = 25.dp, top = 12.dp, bottom = 12.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.LocationOn,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(27.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = location,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                fontWeight = FontWeight.W500,
+                fontSize = 23.sp
+            )
+        }
+    }
+}
+
+@Composable fun AlertsWidget(flood: Boolean, thunder: Boolean, tornado: Boolean) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp),
+        shape = RoundedCornerShape(14.dp),
+        color = MaterialTheme.colorScheme.surfaceContainer,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.padding(start = 25.dp, top = 12.dp, bottom = 12.dp)
+        ) {
+            Text(
+                text = "Active Alerts",
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Medium,
+                fontSize = 23.sp
+            )
+        }
+    }
+}
+
+@Composable fun WarningWidget(icon: Icon, warning: String) {
+    val backgroundColor = when(warning) {
+        "flood" -> Color(0xFF104129)
+        "thunder" -> Color(0xFF5D3E10)
+        "tornado" -> Color(0xFFF7DE93)
+        else -> Color(0xFFFFFFFF)
+    }
+    val iconColor = when(warning) {
+        "flood" -> Color(0xFFA5D396)
+        "thunder" -> Color(0xFFF7DE93)
+        "tornado" -> Color(0xFFFFFFFF)
+        else -> Color(0xFFFFFFFF)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp),
+        shape = RoundedCornerShape(14.dp),
+        color = MaterialTheme.colorScheme.surfaceContainer,
+    ) {
 
     }
 }
