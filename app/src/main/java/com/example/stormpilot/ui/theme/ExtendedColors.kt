8b8c9d94de249b30b8568830db308b0d47647628
sam-grouchnikov <sam.grouchnikov@gmail.com>
2026-06-05 23:15:17 -0400

    Colors

--- app/src/main/java/com/example/stormpilot/ui/theme/ExtendedColors.kt
+++ app/src/main/java/com/example/stormpilot/ui/theme/ExtendedColors.kt
@@ -50,7 +50,7 @@ public class ExtendedColors {
         get() = if (isDark) Color(0xFF533F5F) else Color(0xFFDCBCEA)
 
     public val purpleSurfaceContainer: Color
-        get() = if (isDark) Color(0xFF201A20) else Color(0xFFF8F8F8)
+        get() = if (isDark) Color(0xFF151115) else Color(0xFFF8F8F8)
 
     public val purpleShadow: Color
         get() = if (isDark) Color(0xFF1D1A20) else Color(0xFFD7D1DC)
